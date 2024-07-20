91ffad4399c9e223b78a48d8b894dd4fb957fca7
Acnhor <q619215399@gmail.com>
2024-07-20 14:07:19 +0800

    workflow

--- main.go
+++ main.go
@@ -41,6 +41,9 @@ func main() {
 	// 配置默认运行 Web 服务
 	cmd.RegisterDefaultCmd(rootCmd, cmd.CmdServe)
 
+	// 注册全局参数，--env
+	cmd.RegisterGlobalFlags(rootCmd)
+
 	// 执行主命令
 	if err := rootCmd.Execute(); err != nil {
 		console.Exit(fmt.Sprintf("Failed to run app with %v: %s", os.Args, err.Error()))
