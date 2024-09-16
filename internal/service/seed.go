5ff285e6236ba57447733357fb75dae9c44700eb
Acnhor <q619215399@gmail.com>
2024-09-16 09:10:36 +0800

    添加锁，避免并发问题

--- internal/service/seed.go
+++ internal/service/seed.go
@@ -11,9 +11,7 @@ import (
 	"gorm.io/gorm"
 	"html/template"
 	"math"
-	"math/rand"
 	"sync"
-	"time"
 )
 
 type SeedService struct {
@@ -26,24 +24,6 @@ var mu sync.Mutex
 // address => hSeed
 var usedTempSeeds = make(map[string]string)
 
-// 生成一个唯一的整数ID，支持JavaScript的整数长度
-func (s *SeedService) generateUniqueInt() int64 {
-	mu.Lock()
-	defer mu.Unlock()
-	// 获取当前时间的毫秒级时间戳
-	timestamp := time.Now().UnixNano() / 1e6 // 毫秒级时间戳
-
-	// 生成一个随机数部分
-	// 随机数的范围需要根据时间戳的长度进行调整，以确保结果不超过2^53 - 1
-	rand.Seed(time.Now().UnixNano())
-	randomPart := rand.Int63n(1 << 20) // 生成一个随机数，20位足够大
-
-	// 组合时间戳和随机数，确保结果不超过2^53 - 1
-	uniqueID := (timestamp << 20) | randomPart
-
-	return uniqueID
-}
-
 func (s *SeedService) generateShortID(node *snowflake.Node) int64 {
 	snowflakeID := node.Generate().Int64()
 	jsMaxInt := int64(math.Pow(2, 53) - 1)
@@ -52,6 +32,8 @@ func (s *SeedService) generateShortID(node *snowflake.Node) int64 {
 }
 
 func (s *SeedService) RandomUsableSeed(address string) (string, error) {
+	mu.Lock()
+	defer mu.Unlock()
 	address = dealAddress(address)
 	hSeed, err := hashidsP.HashID.EncodeInt64([]int64{s.generateShortID(snowflakeP.Node)})
 	if err != nil {
